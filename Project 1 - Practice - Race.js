e2ffc510bcc7239201463dbdf9a54fc95db0189e
deanlockley <60471012+deanlockley@users.noreply.github.com>
2020-05-07 12:10:26 +0000

    Update and rename Race.js to Project 1 - Practice - Race.js

--- Project 1 - Practice - Race.js
+++ Project 1 - Practice - Race.js
@@ -6,14 +6,9 @@ let runnerRegisteredTime = 8:00;
 let runnerAge = 18
 
 
-
-
-
 //4. Check runner is an adult and registered early
 
 if (runnerAge > 18) && (runnerRegisteredTime =< 9: 30) {
-
-
 
     //add 1000 to number
 
@@ -21,25 +16,17 @@ if (runnerAge > 18) && (runnerRegisteredTime =< 9: 30) {
 
 }
 
-
-
 //Check race time - runners over 18 who registered early
 
 if (runnerAge > 18) && (runnerRegisteredTime =< 9: 30) {
-
-
 
     console.log('Your race is at 9:30am and your race number is' + raceNumber);
 
 }
 
-
-
 } else if (runnerAge > 18) && (runnerRegisteredTime > 9: 30) {
 
     console.log('Your race is at 11:00am and your race number is' + raceNumber);
-
-
 
 } else {
 
