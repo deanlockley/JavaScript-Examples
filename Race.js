b2be6cc32d0887146ef7ecb0a512246407a3acfc
deanlockley <60471012+deanlockley@users.noreply.github.com>
2020-05-07 12:06:19 +0000

    Add files via upload

---
+++ Race.js
@@ -0,0 +1,48 @@
+// JavaScript source code
+let raceNumber = Math.floor(Math.random() * 1000);
+
+let runnerRegisteredTime = 8:00;
+
+let runnerAge = 18
+
+
+
+
+
+//4. Check runner is an adult and registered early
+
+if (runnerAge > 18) && (runnerRegisteredTime =< 9: 30) {
+
+
+
+    //add 1000 to number
+
+    raceNumber + 1000;
+
+}
+
+
+
+//Check race time - runners over 18 who registered early
+
+if (runnerAge > 18) && (runnerRegisteredTime =< 9: 30) {
+
+
+
+    console.log('Your race is at 9:30am and your race number is' + raceNumber);
+
+}
+
+
+
+} else if (runnerAge > 18) && (runnerRegisteredTime > 9: 30) {
+
+    console.log('Your race is at 11:00am and your race number is' + raceNumber);
+
+
+
+} else {
+
+    console.log('Your race is at 12:30am and your race number is' + raceNumber);
+
+}
